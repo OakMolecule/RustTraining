a00151298e4aacae954ad9daf2b57d7d53498065
OakMolecule <OakMolecule@users.noreply.github.com>
2026-06-18 03:23:21 +0000

    deploy: 41bc2086a467e83b5035e748fa5d54f64b620ee6

--- c-cpp-book/searcher-09f2665d.js
+++ c-cpp-book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1a7771a1.js',
+                path_to_root + 'searchindex-0d148b1a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
